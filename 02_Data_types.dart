ef21d0deca6624f57c198d7bacaf1052204b805d
Ravi Kumar Ray <121496417+Ravilovecode@users.noreply.github.com>
2024-08-06 16:58:32 +0530

    Add files via upload

---
+++ 02_Data_types.dart
@@ -0,0 +1,32 @@
+void main() {
+  // Integers
+  int num1 = 30;
+  var num2 = 30;
+
+  print(num1);
+  print(num2);
+
+  // Numbers Double
+
+  double percnt = 93.4;
+  var percnt2 = 82.56;
+
+  print(percnt);
+  print(percnt2);
+
+  // Strings
+
+  String name1 = "Ravi";
+  var name2 = "Rohan";
+
+  print(name1);
+  print(name2);
+
+  // Boolean
+
+  bool isAlive = true;
+  var isAlive2 = false;
+
+  print(isAlive);
+  print(isAlive2);
+}
