ddef445887edd37afb24641818dd9364a6bc54ab
Ravi Kumar Ray <121496417+Ravilovecode@users.noreply.github.com>
2024-08-08 17:38:24 +0530

    Add files via upload

---
+++ functions.dart
@@ -0,0 +1,14 @@
+void main() {
+  sum(5, 6);
+  print("Area is:" + getarea(5, 6).toString());
+}
+
+void sum(int a, int b) {
+  print(a + b);
+}
+
+int getarea(int l, int b) {
+  return l * b;
+}
+
+
